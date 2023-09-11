50f4e112be2cae13c85d6c3774450b4edfbfc130
jugglingdev <kaylamarie1785@gmail.com>
2023-09-10 21:16:41 -0500

    Finished Exercise 1.2

--- epic-tasks/JS-HandsOn-OOP-This/car.js
+++ epic-tasks/JS-HandsOn-OOP-This/car.js
@@ -6,7 +6,19 @@ class Car {
         this.year = year;
     }
 
-    
-
-
+    displayInfo() {
+        console.log(
+            `Make: ${this.make}` + 
+            `\nModel: ${this.model}` + 
+            `\nYear: ${this.year}`
+        );
+    }
 }
+
+const myCar = new Car('Toyota', 'Camry', 2003);
+myCar.displayInfo();  /* 
+                        Make: Toyota
+                        Model: Camry
+                        Year: 2003
+                      */
+                     
