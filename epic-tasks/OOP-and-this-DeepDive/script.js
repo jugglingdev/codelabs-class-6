e7d6f9ca729bee8b8f03ac2e1724aefa096cae8b
jugglingdev <kaylamarie1785@gmail.com>
2023-09-10 19:40:17 -0500

    Grasped the nuances of 'this' in JavaScript

--- epic-tasks/OOP-and-this-DeepDive/script.js
+++ epic-tasks/OOP-and-this-DeepDive/script.js
@@ -31,14 +31,48 @@ const chico = new Dog('Chico Bean', 'chiweenie', 4, 'brown');
 const lillie = new Dog('Lillie', 'mix breed', 13, 'black');
 
 console.groupCollapsed('1: Basic OOP Principles');
-    console.log(chico);
-    console.log(lillie);
+    console.log(chico);  //  Dog { name: 'Chico Bean', breed: 'chiweenie', age: 4, color: 'brown' }
+    console.log(lillie);  //  Dog { name: 'Lillie', breed: 'mix breed', age: 13, color: 'black' }
 
-    lillie.bark();
-    chico.beg();
-    lillie.wagTail();
-    chico.wagTail();
+    lillie.bark();  //  Lillie: Bark!
+    chico.beg();  //  Chico Bean: Pleeeeeeaaaaaase?!?!?!
+    lillie.wagTail();  //  Lillie's tail is wagging.
+    chico.wagTail();  // Chico Bean's tail is wagging.
 console.groupEnd();
 
 
-// 
+// Understanding 'this' in Context
+
+const person = {
+    firstName: 'Bob',
+    lastName: 'Smith',
+    age: 42,
+    printPerson: function() {
+        console.log(`${this.firstName} ${this.lastName} is ${this.age} years old.`);
+    },
+}
+
+person.printPerson();  // Bob Smith is 42 years old.
+                       // 'this' refers to the 'person' object
+
+const person2 = {
+    firstName: 'Janet',
+    lastName: 'Smith',
+    age: 40,
+    printPerson: function() {
+        
+        const arrowFunction = () => {
+            console.log(`${this.firstName} ${this.lastName} is ${this.age} years old.`);
+        }
+
+        arrowFunction();
+    },
+}
+
+person2.printPerson();  // Jane Smith is 40 years old.
+                        // Arrow functions refer to the nearest function's 'this' scope
+                        // If the arrow function above were not in 'printPerson: function()',
+                        // we would get undefined because 'this' would refer to Window instead of 'person2'
+
+
+                        
