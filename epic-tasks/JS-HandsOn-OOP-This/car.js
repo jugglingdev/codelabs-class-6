734a61f92cdfd8b5149de5187e6d6bad30f46202
jugglingdev <kaylamarie1785@gmail.com>
2023-09-10 22:11:53 -0500

    Finished Exercise 3.1

--- epic-tasks/JS-HandsOn-OOP-This/car.js
+++ epic-tasks/JS-HandsOn-OOP-This/car.js
@@ -21,4 +21,29 @@ myCar.displayInfo();  /*
                         Model: Camry
                         Year: 2003
                       */
-                     
+
+
+class ElectricCar extends Car {
+
+    constructor(make, model, year, batteryLife) {
+        super(make, model, year);
+        this.batteryLife = batteryLife;
+    }
+
+    displayInfo() {
+        console.log(
+            `Make: ${this.make}` + 
+            `\nModel: ${this.model}` + 
+            `\nYear: ${this.year}` 
+        );
+    }
+}
+
+const prius = new ElectricCar('Toyota', 'Prius', 2020, '150,000 miles');
+
+prius.displayInfo();  /*
+                        Make: Toyota
+                        Model: Prius
+                        Year: 2020
+                      */
+
