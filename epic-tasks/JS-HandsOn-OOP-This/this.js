7031fcb85d72f248302c0dfff60f0cf5d1898976
jugglingdev <kaylamarie1785@gmail.com>
2023-09-10 21:39:47 -0500

    Finished Exercise 2.2

--- epic-tasks/JS-HandsOn-OOP-This/this.js
+++ epic-tasks/JS-HandsOn-OOP-This/this.js
@@ -18,3 +18,22 @@ clothing.outfit();  /*
                         accessory: scarf
                     */
 
+const IndianaJonesClothing = {
+
+    hat: 'fedora',
+    shoes: 'boots',
+    accessory: 'whip',
+    outfit: () => {
+        console.log(
+            `hat: ${this.hat}` +
+            `\nshoes: ${this.shoes}` +
+            `\naccessory: ${this.accessory}`
+        );
+    }
+}
+
+IndianaJonesClothing.outfit();  /*
+                        hat: undefined
+                        shoes: undefined
+                        accessory: undefined
+                    */
