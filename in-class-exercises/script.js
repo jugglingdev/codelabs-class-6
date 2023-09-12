5508cf85c42167345a3b8f99798c0d2483080fdc
jugglingdev <kaylamarie1785@gmail.com>
2023-09-11 19:10:40 -0500

    Completed Exercises 2 and 3

--- in-class-exercises/script.js
+++ in-class-exercises/script.js
@@ -1,31 +1,67 @@
 // Exercise 1
 
-function globalFunction() {
-    console.log(`Global Function: ${this}`);
-}
+console.groupCollapsed('Exercise 1');
 
-const myObject = {
-    name: 'Object Method',
-    printThis: function() {
-        console.log(`${this.name}: ${this}`);
+    function globalFunction() {
+        console.log(`Global Function: ${this}`);
     }
-}
 
-const myOtherObject = {
-    name: 'Object Arrow Function',
-    printThis: () => {
-        console.log(`${this.name}: ${this}`);
+    const myObject = {
+        name: 'Object Method',
+        printThis: function() {
+            console.log(`${this.name}: ${this}`);
+        }
     }
-}
 
-globalFunction();  // Global Function: [object global]
-myObject.printThis();  // Object Method: [object Object]
-myOtherObject.printThis();  // undefined: [object Object]
-                            // The arrow function here won't work
+    const myOtherObject = {
+        name: 'Object Arrow Function',
+        printThis: () => {
+            console.log(`${this.name}: ${this}`);
+        }
+    }
+
+    globalFunction();  // Global Function: [object global]
+    myObject.printThis();  // Object Method: [object Object]
+    myOtherObject.printThis();  // undefined: [object Object]
+                                // The arrow function here won't work
+
+console.groupEnd();
 
 // Exercise 2
 
 class Person {
+
+    name;
+    age;
+
+    get name() {
+        return this.name;
+    }
+
+    set name(name) {
+        return this.name = name;
+    }
+
+    greet() {
+        console.log(`Howdy, ${this.name}!`);
+    }
+}
+
+console.groupCollapsed('Exercise 2');
+
+    const mary = new Person();
+    mary.name = 'Mary';
+    mary.greet();
+
+    const gary = new Person();
+    gary.name = 'Gary';
+    gary.greet();
+
+console.groupEnd();
+
+// Exercise 3
+
+class Person2 {
 
     constructor(name, age) {
         this.name = name;
@@ -37,15 +73,15 @@ class Person {
     }
 }
 
-const bud = new Person('Bud', 42);
-const chelsea = new Person('Chelsea', 13);
+console.groupCollapsed('Exercise 3');
 
-bud.greet();  // Howdy, Bud!
-chelsea.greet();  // Howdy, Chelsea!
+    const bud = new Person2('Bud', 42);
+    const chelsea = new Person2('Chelsea', 13);
 
-// Exercise 3
+    bud.greet();  // Howdy, Bud!
+    chelsea.greet();  // Howdy, Chelsea!
 
-
+console.groupEnd();
 
 // Exercise 4
 
